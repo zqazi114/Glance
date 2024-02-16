56c6e66f2170d70117aa88c3bb76ba5b2e711102
Zain Qazi <z.qazi114@gmail.com>
2024-02-16 19:42:48 +0800

    Initial Commit

---
+++ Glance/GlanceApp.swift
@@ -0,0 +1,17 @@
+//
+//  GlanceApp.swift
+//  Glance
+//
+//  Created by Z Q on 2/16/24.
+//
+
+import SwiftUI
+
+@main
+struct GlanceApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
